b50fa96bfb6a48c51dac0be0599221c57aa5cc68
vishalbhalla02 <vishalbhalla722@gmail.com>
2024-07-28 15:29:56 +0530

    trying better

--- src/components/RestaurantCointainer.jsx
+++ src/components/RestaurantCointainer.jsx
@@ -19,7 +19,7 @@ function RestaurantContainer() {
   const fetchData = async () => {
     try {
       const data = await fetch(
-        "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.61450&lng=77.30630&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.61450&lng=77.30630&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
       );
       const json = await data.json();
       const restaurants =
